f6755a16bd54ad9cfee779a2ff747e0805028e01
deepak747 <deepakdubey2050@gmail.com>
2022-08-04 20:02:35 +0530

    change done

--- src/app/service/property.service.ts
+++ src/app/service/property.service.ts
@@ -1,28 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class PropertyService {
-  inserturl="https://test-part1.herokuapp.com/property/add";
-  listurl="https://test-part1.herokuapp.com/property/list";
-  deleteurl="https://test-part1.herokuapp.com/property/delete"
-  constructor(private http:HttpClient) { }
 
+  constructor(private http: HttpClient) { }
 
-       addRecord(data:any):Observable<any>
-       {
-        return this.http.post(this.inserturl,data)
-       }
+  addRecord(data: any): Observable<any> {
+    return this.http.post(environment.inserturl, data)
+  }
 
-       allRecord():Observable<any>
-         {
-          return this.http.get(this.listurl);
-         }
+  allRecord(): Observable<any> {
+    return this.http.get(environment.listurl);
+  }
 
-         deleteRecord(id:any):Observable<any>
-         {
-          return this.http.delete(this.deleteurl+ '/'+id)
-         }
+  deleteRecord(id: any): Observable<any> {
+    return this.http.delete(environment.deleteurl + '/' + id)
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  inserturl:"https://test-part1.herokuapp.com/property/add",
+  listurl:"https://test-part1.herokuapp.com/property/list",
+  deleteurl:"https://test-part1.herokuapp.com/property/delete"
 };
 
 /*
